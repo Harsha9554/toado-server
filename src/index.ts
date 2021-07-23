befe9646d2f2f628395db6e6b27bd87c4c0339f9
Sai SriHarsha Griddaluru <sriharsha.9554@gmail.com>
2021-07-23 18:26:31 +0530

    express-setup-check

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,25 @@
-import express from "express";
+import express, { Application, Response } from "express";
 import cors from "cors";
 
 const main = async () => {
-	const app = express();
-	const PORT = 3000;
+	const app: Application = express();
+	const PORT: number = 3000;
 
 	app.use(cors());
 	app.use(express.json());
+	app.use(express.urlencoded({ extended: true }));
 
-	app.listen(PORT, () => {
-		console.log(`TOAD-O Server running on the port ${PORT}.`);
+	app.get("/", async (res: Response): Promise<Response> => {
+		return res.status(200).send({ message: "TOAD-O" });
 	});
+
+	try {
+		app.listen(PORT, (): void => {
+			console.log(`TOAD-O Server running on the port ${PORT}.`);
+		});
+	} catch (error) {
+		console.error(`Error : ${error}`);
+	}
 };
 
 main();
