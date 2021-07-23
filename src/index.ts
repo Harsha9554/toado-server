002490ee30721f2e54ec00c0f15ba049de1a145b
Sai SriHarsha Griddaluru <sriharsha.9554@gmail.com>
2021-07-23 14:44:05 +0530

    dependencies-added

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("HEY");
+import express from "express";
+import cors from "cors";
+
+const main = async () => {
+	const app = express();
+	const PORT = 3000;
+
+	app.use(cors());
+	app.use(express.json());
+
+	app.listen(PORT, () => {
+		console.log(`TOAD-O Server running on the port ${PORT}.`);
+	});
+};
+
+main();
